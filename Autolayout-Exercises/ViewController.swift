d4747b3b662b951df6ff35d9cd2772f71548d171
Snorlax <lenepouv@gmail.com>
2018-04-04 11:16:53 +0300

    hi there

--- Autolayout-Exercises/ViewController.swift
+++ Autolayout-Exercises/ViewController.swift
@@ -9,17 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var buttonMenu: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var menuHeightConstraint: NSLayoutConstraint!
+    
+    var isMenuOpen: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func actionToggleMenu(_ sender: Any) {
+        isMenuOpen = !isMenuOpen
+        menuHeightConstraint.constant = isMenuOpen ? 200.0 : 60.0
+        titleLabel.text = isMenuOpen ? "Select Item" : "Packing List"
+        
+        UIView.animate(withDuration: 1.0, delay: 0.0,
+                       usingSpringWithDamping: 0.4, initialSpringVelocity: 10.0,
+                       options: .curveEaseIn,
+                       animations: {
+                        self.view.layoutIfNeeded()
+        },
+                       completion: nil
+        )
     }
-
-
 }
 
